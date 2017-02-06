8a62dfd843a162116068de6f37d540a8304c8f83
Jai Singh Sra <jais334@gmail.com>
2017-02-07 00:17:17 +0800

    Numbered destination page and practiced asp.net fundamentals

--- SinExWebApp20326022/App_Start/RouteConfig.cs
+++ SinExWebApp20326022/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace SinExWebApp20326022
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ SinExWebApp20326022/Controllers/TrialController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SinExWebApp20326022.Models;
+
+namespace SinExWebApp20326022.Controllers
+{
+    public class TrialController : Controller
+    {
+        // GET: Trial/Random
+        public ActionResult Random()
+        {
+            var movie = new Trial() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+        
+        // Attribute Route: creates a custom url route that handles multiple parameters with constraints
+        // For more info on constraints, google asp.net mvc attribute route constraints
+        [Route("trial/{pageIndex:regex(\\d{2})}/{sortBy}")]
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
